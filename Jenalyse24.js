ff10cd33198ba8fc4ef6ba83efc4840362f24f62
Jenalyse13 <144177829+Jenalyse13@users.noreply.github.com>
2024-05-20 23:00:18 -0400

    Update Jenalyse24.js

--- Jenalyse24.js
+++ Jenalyse24.js
@@ -6,11 +6,32 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Add a marker to the map for New York City
-var marker = L.marker([40.683311452523625, -73.97619537068677]).addTo(map);
+// 1A Add a marker to the map for New York City
+var baraclays_pizza = L.marker([40.683311452523625, -73.97619537068677]).addTo(map);
 
-// Add a popup to the marker
-marker.bindPopup("<b>Fini Pizza Barclays Plaza Downtown Brooklyn</b><br>This is a Leaflet map.").openPopup();
+// 1B Add a popup to the marker
+baraclays_pizza.bindPopup("<b>Fini Pizza Barclays Plaza Downtown Brooklyn</b><br>This is a Leaflet map.").openPopup();
+
+// 2A Add a marker to the map for New York City
+var paneorama = L.marker([40.69426080526414, -73.84961585947472]).addTo(map);
+
+// 2B Add a popup to the marker
+paneorama.bindPopup("<b>Paneorama Bakery & Pastry Shop</b><br>This is a Leaflet map.").openPopup();
+
+
+// 3A Add a marker to the map for New York City
+var south_ferry = L.marker([40.70139399691619, -74.01330340207514]).addTo(map);
+
+// 3B Add a popup to the marker
+south_ferry.bindPopup("<b>Staten Island South Ferry</b><br>This is a Leaflet map.").openPopup();
+
+// 4A Add a marker to the map for New York City
+var battery_park = L.marker([40.70294229941433, -74.01536271731277]).addTo(map);
+
+// 4B Add a popup to the marker
+battery_park.bindPopup("<b>The Battery Park</b><br>This is a Leaflet map.").openPopup();
+
+
 
 // Load the GeoJSON line file
 fetch('https://jenalyse13.github.io/GEG-212/Jenalyse_daytimeroute.geojson')
